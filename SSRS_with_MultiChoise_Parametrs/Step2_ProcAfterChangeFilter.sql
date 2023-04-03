0f82f979774a6e9209cae22ecf518ed2b56f1e91
Igor Luca <89498870+IgorLuka1976@users.noreply.github.com>
2023-04-03 20:13:05 +0300

    Update Step2_ProcAfterChangeFilter.sql

--- SSRS_with_MultiChoise_Parametrs/Step2_ProcAfterChangeFilter.sql
+++ SSRS_with_MultiChoise_Parametrs/Step2_ProcAfterChangeFilter.sql
@@ -1,7 +1,7 @@
 -----Important
 USE [AdventureWorks2019]
 GO
-/****** Object:  StoredProcedure [dbo].[testForSSRS]    Script Date: 03.04.2023 19:19:46 ******/
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
